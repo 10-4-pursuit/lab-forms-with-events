b069b02ea0d2480777cc78b90ad36132bab39e14
GyancarlosPinto <gyancarlospinto@pursuit.org>
2023-11-02 01:17:26 -0400

    Finished Forms with Events Lab

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-console.log("Code your solution!")
+
+function toDoList() {
+    const formElement = document.querySelector("#form");
+    formElement.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const newToDo = formElement.newToDo.value;
+        const errorMessage = document.querySelector("p");
+        const getToDoList = document.querySelector("ul");
+
+        if (newToDo === "") {
+            errorMessage.textContent = "Your To Do Entry Cannot Be Empty!"
+        } else {
+
+            const lines = newToDo.split("\n");
+
+            formElement.reset();
+
+            for (const line of lines) {
+
+                const deleteButton = document.createElement("button");
+                deleteButton.textContent = "Delete";
+
+                deleteButton.addEventListener("click", (event) => {
+                    event.preventDefault();
+                    entry.remove();
+                });
+
+                const entry = document.createElement("li");
+                entry.textContent = line;
+                entry.append(deleteButton);
+                
+
+                entry.addEventListener("click", (event) => {
+                    event.preventDefault();
+                    if (entry.style.textDecoration ===  "line-through") {
+                        entry.style.textDecoration = "none";
+                    } else {
+                        entry.style.textDecoration = "line-through";
+                    }
+                });
+
+                getToDoList.append(entry);
+            }
+        }
+    });
+};
+
+toDoList();
