a334840c36ab7c2c275b3355510b12063e491c21
ada lovelace <williambookman@pursuit.org>
2023-11-01 20:47:47 -0400

    lab setup plus

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+// this creates a A single `ul` tag, empty when the page is first loaded.
+
+window.onload = function() {
+    var ul = document.createElement('ul');
+    ul.id = 'my-ul';
+    ul.setAttribute ('name', 'first-ul');
+    document.body.appendChild(ul);
+};
+
 console.log("Code your solution!")
