2bed53bac518144e749014e0d936a3f27b31b0ae
ada lovelace <williambookman@pursuit.org>
2023-11-08 16:16:32 -0500

    added delete button bonus

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // this creates a A single `ul` tag, empty when the page is first loaded.
-
 window.onload = function () {
   const ul = document.createElement("ul");
   ul.id = "ul";
@@ -7,68 +6,68 @@ window.onload = function () {
   document.body.appendChild(ul);
 
   const ulElement = document.querySelectorAll("#ul");
+};
 
-  };
+//this creates an error message element
+const errorMessage = document.createElement("p");
+errorMessage.id = "error-message";
+errorMessage.innerText = "Error. Todo cannot be empty";
+document.body.appendChild(errorMessage);
 
-  //this creates an error message element
-  const errorMessage = document.createElement("p");
-  errorMessage.id = "error-message";
-  errorMessage.innerText = "Error. Todo cannot be empty";
-  document.body.appendChild(errorMessage);
-
-  /** When the user writes something in the `form`'s text input area and
+/** When the user writes something in the `form`'s text input area and
 clicks `submit`, the `ul` should update with a new `li` item at the bottom of the list. */
-  function addListItem(form) {
-    // get the value of the text input area
-    const inputValue = form.querySelector('input[type="text"]').value;
+function addListItem(form) {
+  // get the value of the text input area
+  const inputValue = form.querySelector('input[type="text"]').value;
 
-    // Get the error message element
-    const errorMessage = document.querySelector("#error-message");
+  // Get the error message element
+  const errorMessage = document.querySelector("#error-message");
 
-    // If the input value is empty, display the error message and prevent the form from submitting
-    if (!inputValue) {
-      errorMessage.style.display = "block";
-      event.preventDefault();
-    } else {
-      errorMessage.style.display = "none";
-    }
-
-    if (inputValue) {
-      // appends inputValue to li
-      const li = document.createElement("li");
-      li.textContent = inputValue;
-
-      //adds li to UL
-      const ul = document.querySelector("#ul");
-      ul.appendChild(li);
-
-      li.addEventListener("dblclick", removeListItem(ul))
-    }
-    // Reset the value of the text input area
-    document.querySelector("input[type='text']").value = "";
+  // If the input value is empty, display the error message and prevent the form from submitting
+  if (!inputValue) {
+    errorMessage.style.display = "block";
+    event.preventDefault();
+  } else {
+    errorMessage.style.display = "none";
   }
 
+  if (inputValue) {
+    // appends inputValue to li
+    const li = document.createElement("li");
+    li.textContent = inputValue;
 
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
 
-  //this calls the above function when the submit button is pressed on the form
-  document.querySelector("form").addEventListener("submit", (event) => {
-    event.preventDefault();
-    addListItem(event.target);
-  });
+    li.appendChild(deleteButton); 
 
+    //adds li to UL. This also allows us add an event listener to all "li"
+    const ul = document.querySelector("#ul");
+    ul.appendChild(li);
 
-  function removeListItem(ul){
-    //select item you want removed
-   ul.addEventListener("dblclick", (event) => {
+    li.addEventListener("dblclick", removeListItem(ul));
+
+    deleteButton.addEventListener("click", () => {
+      ul.removeChild(li);
+    })
+  }
+  // Reset the value of the text input area (**bonus)
+  document.querySelector("input[type='text']").value = "";
+}
+
+//this calls the above function when the submit button is pressed on the form
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  addListItem(event.target);
+});
+
+function removeListItem(ul) {
+  //select item you want removed
+  ul.addEventListener("dblclick", (event) => {
     const li = event.target;
 
-    li.style.textDecoration = "line-through"
-   
-      // ul.removeChild(li)
-      
-   })
-  }
+    li.style.textDecoration = "line-through";
 
-
-
- 
+    // ul.removeChild(li)
+  });
+}
