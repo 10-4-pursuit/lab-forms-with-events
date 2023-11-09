a1749762c55a405c5a441d1bb50fe503727a47c7
EdgarCollado <edgarcollado@pursuit.org>
2023-11-08 21:00:50 -0500

    lab-submission

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-const formElement = document.querySelector("#form");
+const formElement = document.querySelector("form");
+
+const toDoListInput = document.querySelector("ul");
+
+formElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const newToDoAddition = formElement.toDos.value;
+  const errorMessage = document.querySelector(".error-message");
+
+  const entry = document.createElement("li");
+  entry.addEventListener("click", (ev) => {
+    ev.preventDefault();
+    if (ev.target.style.textDecoration === "line-through") {
+      ev.target.style.textDecoration = "none";
+    } else {
+      ev.target.style.textDecoration = "line-through";
+    }
+  });
+  entry.textContent = newToDoAddition;
+  toDoListInput.appendChild(entry);
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.style.margin = "4px";
+
+  deleteButton.addEventListener("click", (ev) => {
+    ev.preventDefault();
+    entry.remove();
+  });
+  entry.append(deleteButton);
+  formElement.reset();
+});
+
+for (li of toDoListInput.children) {
+  li.append(deleteButton);
+}
