cb34c579a18247734e9838e435461ab421788387
AmiraFredericks <amirafredericks@pursuit.org>
2023-12-03 18:39:04 -0500

    To Do List

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("Code your solution!")
+document.addEventListener("DOMContentLoaded", function () {
+    const myTodoForm = document.getElementById("my-todo-form");
+    const myTodoList = document.getElementById("my-todo-list");
+    const errorMessage = document.getElementById("error-message");
+    const myTodoInput = document.getElementById("todo-input");
+
+    myTodoForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+        const todoText = myTodoInput.value;
+        
+        if (todoText === "" ) {
+            errorMessage.textContent = "Please enter a todo.";
+        } else {
+            errorMessage.textContent = "";
+
+            const listItem = document.createElement("li");
+            listItem.textContent = todoText;
+
+            listItem.addEventListener("click", function () {
+                
+                if (listItem.classList.contains("completed")) {
+                    listItem.classList.add("completed");
+                } else {
+                    listItem.classList.remove("completed");  
+                }
+            });
+            myTodoList.appendChild(listItem);
+            myTodoInput.value = "";
+        }
+    });
+
+});
+
